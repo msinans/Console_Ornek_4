8f7509e0f210c29f570f3e540a960bc817b92358
m.sinans <msomercikoglu@gmail.com>
2023-03-11 15:22:06 +0300

    Add project files.

---
+++ Console_Ornek_4/Islem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_Ornek_4
+{
+    internal class Islem
+    {
+        public int topla(int s1, int s2)
+        {
+            int s3 = s1 + s2;
+            Console.WriteLine("Sonuç: " + s3);
+            return s3;
+        }
+    }
+}
---
+++ Console_Ornek_4/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_Ornek_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Islem islem = new Islem();
+            islem.topla(7, 8);
+            islem.topla(9, 10);
+            Console.Read();
+        }
+    }
+}
